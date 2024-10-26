b2c361fa3675ad5b509c5c8e83ec7de87a405f77
Anastasia Karpova <vip.anas2003@inbox.ru>
2024-10-26 21:46:41 +0300

    Danger detection added (instead of counting trains!!!)

--- detection_server/main.py
+++ detection_server/main.py
@@ -3,7 +3,8 @@ import tkinter as tk
 from tkinter import ttk, filedialog
 import cv2
 import PIL.Image, PIL.ImageTk
-from ultralytics import YOLO
+from inference import get_model
+
 import supervision as sv
 from typing import Callable
 import numpy as np
@@ -36,8 +37,8 @@ logger = logging.getLogger('urbanGUI')
 
 locale.setlocale(locale.LC_ALL, 'Russian_Russia.1251')
 
-# Загрузите вашу модель
-model = YOLO("best.pt")
+model = get_model(model_id="final-zjnyf/5", api_key="oMwwId6tzG8Aga5aGVo2")
+
 
 window = tk.Tk()
 window.title("Видеообработчик")
@@ -50,7 +51,7 @@ progress = ttk.Progressbar(window, orient="horizontal", length=300, mode='determ
 progress.pack(side=tk.TOP, padx=20, pady=20)
 
 class ObjectTracker:
-    def __init__(self, max_distance=30):
+    def __init__(self, max_distance=50):
         self.max_distance = max_distance
         self.tracked_objects = {}
         self.next_id = 1
@@ -58,6 +59,11 @@ class ObjectTracker:
 
     def update(self, detections):
         current_detections = detections.xyxy
+        danger_flag = False
+        if len(list(current_detections)) > 2: # Если больше 3 людей в моменте для наглядности
+            print(f"Danger, {len(list(current_detections))} people")
+            danger_flag = True
+
         for i, detection in enumerate(current_detections):
             x1, y1, x2, y2 = detection
             center_x = (x1 + x2) / 2
@@ -83,6 +89,7 @@ class ObjectTracker:
                 obj_class = detections.data['class_name'][i]
                 self.object_classes[self.next_id] = obj_class
                 self.next_id += 1
+        return danger_flag
 
     def get_object_counts(self):
         class_counts = {}
@@ -101,70 +108,24 @@ def load_video():
     file_path = filedialog.askopenfilename()
     if file_path:
         update_video_date(file_path)
-        threading.Thread(target=lambda: process_video(file_path, "output.mp4", process_frame), daemon=True).start()
+        threading.Thread(target=lambda: process_video(file_path, "digital_breakthrough_dispatch_control/output.mp4", process_frame), daemon=True).start()
 
 
 def process_frame(frame: np.ndarray, test: int) -> np.ndarray:
     global previous_frame
     global tracker
 
-    results = model(frame, imgsz=1280)[0]
+    results = model.infer(frame)[0]
 
-    detections = sv.Detections.from_ultralytics(results)
+    detections = sv.Detections.from_inference(results)
 
-    tracker.update(detections)
+    danger = tracker.update(detections)
+    if (danger):
+        print(f"Danger detected at {test} frame, thats {test/70} second") # fps
 
     print("New detection:", tracker.get_object_counts())
 
-    # print(detections.data['class_name'])
-    # lst = detections.data['class_name']
-    # a = list(map(lambda x: x.replace("np.str_('", '').replace("')", ''), lst))
-    #
-    # for item in a:
-    #     dictionary[item] = dictionary.get(item, 0) + 1
-    # if dictionary == previous_frame:
-    #     print(True)
-    # else:
-    #     set1 = set(dictionary.items())
-    #     set2 = set(previous_frame.items())
-    #     diff_res = list(set1 ^ set2)
-    #     logger.info(diff_res)
-    #
-    #     unique_diff_res = {}
-    #
-    #     for item in diff_res:
-    #         if item[0] in unique_diff_res:
-    #             unique_diff_res[item[0]].append(item[1])
-    #         else:
-    #             unique_diff_res[item[0]] = [item[1]]
-    #
-    #     # Преобразуем словарь в список с разницей между максимальным и минимальным значениями
-    #     unique_diff_list = [(key, max(values) - min(values)) for key, values in unique_diff_res.items()]
-    #
-    #     if (len(diff_res) > 1): # То есть прям жесткая ошибка пошла, два класса полетело
-    #         # Применяем значения из unique_diff_list к final_count
-    #         for key, value in unique_diff_list:
-    #             if key in final_count:
-    #                 final_count[key] += value
-    #             else:
-    #                 final_count[key] = dictionary[key]
-    #         print("CRITICAL")
-    #     elif (len(diff_res) == 1) and (diff_res[0][-1] > 2): # или в одном классе появилось много вагонов:
-    #         final_count[[0][0]] = diff_res[0][-1]
-    #         print("CRITICAL")
-
-
-
-    # logger.info(dictionary)
-    # print(dictionary)
-    # print(test)
-    # print("out of " + str(final_count))
-    # previous_frame = dictionary
-
     box_annotator = sv.BoundingBoxAnnotator()
-
-
-
 
     label_annotator = sv.LabelAnnotator(text_color=sv.Color.BLACK)
 
@@ -188,10 +149,10 @@ def process_video(
     VideoInfo = sv.VideoInfo.from_video_path(source_path)
     source_video_info = VideoInfo.from_video_path(video_path=source_path)
     frame_count = int(cv2.VideoCapture(source_path).get(cv2.CAP_PROP_FRAME_COUNT))
-    progress['maximum'] = frame_count/50 # stride
+    progress['maximum'] = frame_count/20 # stride
     with sv.VideoSink(target_path=target_path, video_info=source_video_info) as sink:
         for index, frame in enumerate(
-            sv.get_video_frames_generator(source_path=source_path, stride=50) #50 frames skip
+            sv.get_video_frames_generator(source_path=source_path, stride=20) #20 frames skip
         ):
             result_frame = callback(frame, index)
             if index % sink.video_info.fps == 0:
